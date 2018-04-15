ca9613c3e7a70c168bb2df9d1b8ed05c08c7e34e
motoki-shun <motoki-shun@kayac.com>
2018-04-15 18:10:57 +0900

    moveBlockDown, validate

--- tetris.go
+++ tetris.go
@@ -18,7 +18,8 @@ func (tetris *Tetris) newGame() {
 }
 
 func (tetris *Tetris) update() {
-  tetris.currentBlock.moveDown()
+  // tetris.currentBlock.moveDown()
+  tetris.moveBlockDown()
 }
 
 func (tetris *Tetris) initBoard() {
@@ -26,4 +27,40 @@ func (tetris *Tetris) initBoard() {
   for r := 0; r < logical_rows; r++ {
     tetris.board[r] = make([]int, cols)
   }
+}
+
+func (tetris *Tetris) moveBlockDown() bool {
+  // isValid := tetris.validate(0, 1)
+  isValid := tetris.validate(0, 1, tetris.currentBlock)
+  if isValid {
+    tetris.currentBlock.moveDown()
+  }
+  return isValid
+}
+
+func (tetris *Tetris) validate(offsetX, offsetY int, block *Block) bool {
+  // block = block || tetris.currentBlock
+  nextX := block.x + offsetX
+  nextY := block.y + offsetY
+  if block.shape == nil {
+    return false
+  }
+  for y := 0; y < number_of_block; y++ {
+    for x := 0; x < number_of_block; x++ {
+      if block.shape[y][x] == 0 {
+        continue
+      }
+      boardX := x + nextX
+      boardY := y + nextY
+      isOutsideLeftWall := boardX < 0
+      isOutsideRightWall := boardX >= cols
+      isUnderBottom := boardY >= logical_rows
+      isOutsideBoard := boardY >= len(tetris.board) || boardX >= len(tetris.board[boardY])
+      isExistsBlock := !isOutsideBoard && tetris.board[boardY][boardX] != 0
+      if isOutsideLeftWall || isOutsideRightWall || isUnderBottom || isOutsideBoard || isExistsBlock {
+        return false
+      }
+    }
+  }
+  return true
 }
